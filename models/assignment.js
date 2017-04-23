7f1dd63f27e6f4199b9c8ca7528b14bd396a6236
ducistheboss <alex.le@yale.edu>
2017-04-23 10:53:33 -0400

    init grading

--- models/assignment.js
+++ models/assignment.js
@@ -15,16 +15,4 @@ const assignmentSchema = new Schema({
   grade: String
 });
 
-// This method will be responsible for task completion.
-// TaskSchema.methods.completeTask = function(err) {
-// 	if(!err) {
-// 		this.isComplete = !(this.isComplete);
-// 		this.save();
-// 	}
-// 	else {
-// 		console.log('Error completing a task.');
-// 	}
-// 	return;
-// };
-
 module.exports = mongoose.model('Assignment', assignmentSchema);
--- server.js
+++ server.js
@@ -1,28 +1,23 @@
 'use strict';
-
 const express = require('express');
 const bodyParser = require('body-parser');
 const session = require('express-session');
 const MongoDBStore = require('connect-mongodb-session')(session);
 const mongoose = require('mongoose');
 const validator = require('validator');
-
-// configuration ===============================================================
 const configDB = require('./config/database.js');
 mongoose.connect(configDB.url); // connect to our database
 
 const app = express();
 const port = process.env.PORT || 4000;
-
 app.use(bodyParser.json()); // get information from html forms
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
-
 app.set('view engine', 'ejs'); // set up ejs for templating
 app.use(express.static(__dirname + '/public'));
 
-var Users = require('./models/user.js');
-var Assignment = require('./models/assignment.js');
+const Users = require('./models/user.js');
+const Assignment = require('./models/assignment.js');
 
 // Configure our app
 const store = new MongoDBStore({
@@ -30,62 +25,65 @@ const store = new MongoDBStore({
   collection: 'sessions',
 });
 
-app.use(bodyParser.urlencoded({
-  extended: true,
-}));
+  // manage sessions
+  app.use(session({
+    secret: 'thisisasecretsession',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+      secure: 'auto',
+    },
+    store,
+  }));
 
-app.use(session({
-  secret: 'thisisasecretsession',
-  resave: false,
-  saveUninitialized: true,
-  cookie: {
-    secure: 'auto',
-  },
-  store,
-}));
-
-function isLoggedIn(req, res, next) {
-	if(res.locals.currentUser) {
-		next();
-	}
-
-}
-
-app.use(function(req, res, next) {;
-	if(req.session.userId) {
-		Users.findById(req.session.userId, function(err, user) {
-			if(!err) {
-				res.locals.currentUser = user;
-			}
-			next();
-		});
-	}
-	else {
-		next();
-	}
-});
-
-function loadStudentAssignments(req, res, next) {
-	if(!res.locals.currentUser) {
-		return next();
-	}
-
-	Assignment.find({ $or:[
-			{owner: res.locals.currentUser}
-		]}, function(err, assignment) {
-      if(!err) {
-  			res.locals.assignment = assignment;
-  		}
-  		else {
-  			res.render('index', { errors: 'Error loading task.'} );
-  		}
+  app.use(function(req, res, next) {;
+  	if(req.session.userId) {
+  		Users.findById(req.session.userId, function(err, user) {
+  			if(!err) {
+  				res.locals.currentUser = user;
+  			}
+  			next();
+  		});
+  	} else {
   		next();
-	  }
-  );
-}
+  	}
+  });
 
-app.get('/', loadStudentAssignments , function (req, res) {
-	res.render('index.ejs');
+  // middleware
+    function isLoggedIn(req, res, next) {
+    	if(res.locals.currentUser) {
+    		next();
+    	}
+    }
+
+    // load student assignments
+    function loadStudentAssignments(req, res, next) {
+    	if(!res.locals.currentUser) {
+    		return next();
+    	}
+    	Assignment.find({owner: res.locals.currentUser}, function(err, assignment) {
+        if(!err) {
+          res.locals.assignment = assignment;
+      	} else {
+      		res.render('index', { errors: 'Error loading task.'} );
+      	}
+        next();
+      });
+    }
+
+    // load all submitted assignment
+    function loadAssignments(req, res, next) {
+    	Assignment.find(function(err, assignment) {
+        if(!err) {
+      		res.locals.assignment = assignment;
+      	}
+        next();
+      });
+    }
+
+// user route
+app.get('/', loadStudentAssignments, function (req, res) {
+  res.render('index');
 });
 
   // User handle
@@ -93,16 +91,16 @@ app.get('/', loadStudentAssignments , function (req, res) {
     // Login
     app.post('/user/login', function (req, res) {
     	var user = Users.findOne({email: req.body.email}, function(err, user) {
+        // return errors if user email not found
     		if(err || !user) {
     			res.render('index', {errors: "Invalid email address"});
     			return;
     		}
 
+        // compare password
         user.comparePassword(req.body.password, function(err, isMatch) {
     			if(err || !isMatch){
     				res.render('index', {errors: 'Invalid password'});
-    				console.log('\n\nInvalid password.\n\n');
-    				// res.render('index', {errors: 'Invalid password'});
     				return;
     	   		}
     		   	else{
@@ -139,16 +137,13 @@ app.get('/', loadStudentAssignments , function (req, res) {
     	newUser.email = req.body.email;
     	newUser.hashed_password = req.body.password;
       newUser.type = "student";
-
       newUser.save(function(err, user){
-
         if(user && !err){
           req.session.userId = user._id;
           res.redirect('/');
           return;
       	}
       	var errors = "Error registering you.";
-
       });
 
     // end of Register post
@@ -166,52 +161,63 @@ app.get('/', loadStudentAssignments , function (req, res) {
   // Assignment Management
   app.use(isLoggedIn);
 
-    // Submit assignment
-    app.post('/assignment/submit', function (req, res) {
-    	var newAssignment = new Assignment();
-    	newAssignment.owner = res.locals.currentUser;
-      newAssignment.student = req.body.student;
-    	newAssignment.name = req.body.name;
-      newAssignment.comment = req.body.comment;
-    	newAssignment.detail = req.body.detail;
-      newAssignment.date = new Date();
-      newAssignment.save(function(err, task) {
-        if(task && !err){
-          if(err || !task) {
-      			res.render('index', { errors: 'Error saving task to the database.'} );
+  // for grader
+
+    // see assignments
+    app.get('/admin', loadAssignments,function (req, res) {
+      if (res.locals.currentUser.type == "admin") {
+        res.render('admin');
+      } else {
+        res.redirect('/');
+      }
+    });
+
+    // for student
+
+      // Submit assignment
+      app.post('/assignment/submit', function (req, res) {
+      	var newAssignment = new Assignment();
+      	newAssignment.owner = res.locals.currentUser;
+        newAssignment.student = req.body.student;
+      	newAssignment.name = req.body.name;
+        newAssignment.comment = req.body.comment;
+      	newAssignment.detail = req.body.detail;
+        newAssignment.date = new Date();
+        newAssignment.save(function(err, assignment) {
+          if(err || !assignment) {
+      			res.render('index', { errors: 'Error saving assignment to the database.'} );
       		} else {
       			res.redirect('/');
       		}
-        }
+        });
       });
-    });
 
-    // Delete an assignment
-    app.post('/assignment/:id/delete', function(req, res) {
-    	Assignment.findById(req.params.id, function(err, assignmentToRemove) {
-    		if(err || !assignmentToRemove) {
-    			console.log('Error finding assignment on database.');
-    			res.redirect('/');
-    		}
-    		else {
-    			assignmentToRemove.remove();
-    			res.redirect('/');
-    		}
-    	});
-    });
+      // Delete an assignment
+      app.post('/assignment/:id/delete', function(req, res) {
+      	Assignment.findById(req.params.id, function(err, assignmentToRemove) {
+      		if(err || !assignmentToRemove) {
+      			console.log('Error finding assignment on database.');
+      			res.redirect('/');
+      		}
+      		else {
+      			assignmentToRemove.remove();
+      			res.redirect('/');
+      		}
+      	});
+      });
 
-    // See assignment detail
-    app.get('/assignment/:id/detail', function(req, res) {
-    	Assignment.findById(req.params.id, function(err, assignmentToView) {
-    		if(err || !assignmentToView) {
-    			console.log('Error finding assignment on database.');
-    			res.redirect('/');
-    		}
-    		else {
-    			res.render('detail', { 'assignment': assignmentToView})
-    		}
-    	});
-    });
+      // See assignment detail
+      app.get('/assignment/:id/detail', function(req, res) {
+      	Assignment.findById(req.params.id, function(err, assignmentToView) {
+      		if(err || !assignmentToView) {
+      			console.log('Error finding assignment on database.');
+      			res.redirect('/');
+      		}
+      		else {
+      			res.render('detail', { 'assignment': assignmentToView})
+      		}
+      	});
+      });
 
 
 // server start
